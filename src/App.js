43a369aaed2bff57c1b6ed3b87ed31d7cf3f5877
Isabela Moreira <isabela.cmor@gmail.com>
2018-12-01 14:03:14 -0800

    added variants

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 import BurgerMenu from 'react-burger-menu';
 import { Experiment, Variant, emitter, experimentDebugger } from '@marvelapp/react-ab-test';
 import Mixpanel from 'mixpanel';
-import secrets from '../secrets.json';
+import secrets from './secrets.json';
+
+var VariantsEnum = Object.freeze({"control":'control', "variant01":'notifications', "variant02":'myFavorites'})
+
+experimentDebugger.enable();
+emitter.defineVariants('hamburgerMenuIconExperiment', [ VariantsEnum.control, VariantsEnum.variant01, VariantsEnum.variant02], [34, 33, 33]);
+var mixpanel = Mixpanel.init(secrets.mixpanelToken);
 
 class MenuWrap extends Component {
   constructor (props) {
@@ -53,8 +59,12 @@ class App extends Component {
     };
   }
 
-  getItems() {
-    let items = [
+  getItems(variantName) {
+    let items;
+
+    switch (variantName) {
+      case VariantsEnum.control:
+        items = [
           <a key="0" href=""><i className="fa fa-fw fa-star-o" /><span>Favorites</span></a>,
           <a key="1" href=""><i className="fa fa-fw fa-bell-o" /><span>Alerts</span></a>,
           <a key="2" href=""><i className="fa fa-fw fa-envelope-o" /><span>Messages</span></a>,
@@ -62,13 +72,40 @@ class App extends Component {
           <a key="4" href=""><i className="fa fa-fw fa-bar-chart-o" /><span>Analytics</span></a>,
           <a key="5" href=""><i className="fa fa-fw fa-newspaper-o" /><span>Reading List</span></a>
         ];
+        break;
+
+      case VariantsEnum.variant01:
+        items = [
+          <a key="0" href=""><i className="fa fa-fw fa-star-o" /><span>Favorites</span></a>,
+          <a key="1" href=""><i className="fa fa-fw fa-bell-o" /><span>Notifications</span></a>,
+          <a key="2" href=""><i className="fa fa-fw fa-envelope-o" /><span>Messages</span></a>,
+          <a key="3" href=""><i className="fa fa-fw fa-comment-o" /><span>Comments</span></a>,
+          <a key="4" href=""><i className="fa fa-fw fa-bar-chart-o" /><span>Analytics</span></a>,
+          <a key="5" href=""><i className="fa fa-fw fa-newspaper-o" /><span>Reading List</span></a>
+        ];
+        break;
+
+      case VariantsEnum.variant02:
+        items = [
+          <a key="0" href=""><i className="fa fa-fw fa-star-o" /><span>My favorites</span></a>,
+          <a key="1" href=""><i className="fa fa-fw fa-bell-o" /><span>Alerts</span></a>,
+          <a key="2" href=""><i className="fa fa-fw fa-envelope-o" /><span>Messages</span></a>,
+          <a key="3" href=""><i className="fa fa-fw fa-comment-o" /><span>Comments</span></a>,
+          <a key="4" href=""><i className="fa fa-fw fa-bar-chart-o" /><span>Analytics</span></a>,
+          <a key="5" href=""><i className="fa fa-fw fa-newspaper-o" /><span>Reading List</span></a>
+        ];
+        break;
+    
+      default:
+        break;
+    }
 
     return items;
   }
 
-  getMenu() {
+  getMenu(variantName) {
     const Menu = BurgerMenu[this.state.currentMenu];
-    const items = this.getItems();
+    const items = this.getItems(variantName);
     let jsx = (
         <MenuWrap wait={20}>
           <Menu id={this.state.currentMenu} pageWrapId={'page-wrap'} outerContainerId={'outer-container'}>
@@ -83,7 +120,17 @@ class App extends Component {
   render() {
     return (
       <div id="outer-container" style={{height: '100%'}}>
-        {this.getMenu()}
+        <Experiment name='hamburgerMenuIconExperiment'>
+          <Variant name={VariantsEnum.control}>
+            {this.getMenu(VariantsEnum.control)}
+          </Variant>
+          <Variant name={VariantsEnum.variant01}>
+            {this.getMenu(VariantsEnum.variant01)}
+          </Variant>
+          <Variant name={VariantsEnum.variant02}>
+            {this.getMenu(VariantsEnum.variant02)}
+          </Variant>
+        </Experiment>
         <main id="page-wrap">
           <h1><a href="https://github.com/negomi/react-burger-menu">react-burger-menu</a></h1>
           <h2 className="description">An off-canvas sidebar React component with a collection of effects and styles using CSS transitions and SVG path animations.</h2>
